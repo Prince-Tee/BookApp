66110e2b5e61c1fb8eb406be798af187e1c66b78
Taiwo <102447760+Prince-Tee@users.noreply.github.com>
2022-11-01 04:19:30 +0100

    Update App.js
    A bookApplist

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import { ActivityIndicator, FlatList, Text, View } from 'react-native'
+import React from 'react';
+import { SectionList, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  const [isLoading, setLoading] = useState(true)
-  const [data, setData] = useState([])  
- 
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
 
-  return (
-    <View style={{ flex: 1, padding: 24 }}>
-    </View>)
- 
+const SectionListBasics = () => {
+    return (
+      <View style={styles.container}>
+        <SectionList
+          sections={[
+            {title: 'War books', data: ['The fighting spirit', 'African Warrior', 'The land of horror']},
+            {title: 'love books', data: ['The Rose', 'Died for Lily', 'The God is alive', 'Queen and the Beast', 'John the loverboy', 'Julie']},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+          keyExtractor={(item, index) => `basicListEntry-${item.title}`}
+        />
+      </View>
+    );
 }
+
+export default SectionListBasics;
 
